dbc1b54f703558c3404e3f56a9fc3610798df3c5
Marat Dolotov <marat@Mac-mini-Marat.local>
2025-06-26 21:46:48 +0500

    community

--- lua/community.lua
+++ lua/community.lua
@@ -6,5 +6,17 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.search.nvim-spectre" },
+  { import = "astrocommunity.completion.codeium-nvim" },
+  { import = "astrocommunity.completion.codeium-vim" },
+  { import = "astrocommunity.editing-support.refactoring-nvim" },
+  { import = "astrocommunity.editing-support.vim-visual-multi" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  -- { import = "astrocommunity.editing-support.chatgpt-nvim" },
+  -- { import = "astrocommunity.recipes.heirline-mode-text-statusline" },
+  { import = "astrocommunity.completion.avante-nvim" },
+  { import = "astrocommunity.recipes.heirline-nvchad-statusline" },
+  { import = "astrocommunity.pack.php" },
   -- import/override with your plugins folder
 }
